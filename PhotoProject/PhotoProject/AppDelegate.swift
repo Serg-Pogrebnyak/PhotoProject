f59c98299a0324eb56bb221ceef5b6fae6a4e69e
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-04-08 10:40:02 +0300

    added empty project

---
+++ PhotoProject/PhotoProject/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PhotoProject
+//
+//  Created by Sergey Pohrebnuak on 08.04.2020.
+//  Copyright © 2020 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ PhotoProject/PhotoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoProject
+//
+//  Created by Sergey Pohrebnuak on 08.04.2020.
+//  Copyright © 2020 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
