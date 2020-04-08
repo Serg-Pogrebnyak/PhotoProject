37e5f88e0aefafb019a83cf196790c7815f81549
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-04-08 11:14:38 +0300

    configure collection view

---
+++ PhotoProject/PhotoProject/PhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoProject
+//
+//  Created by Sergey Pohrebnuak on 08.04.2020.
+//  Copyright © 2020 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- PhotoProject/PhotoProject/ViewController.swift
+++ PhotoProject/PhotoProject/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet fileprivate weak var photoCollectionView: UICollectionView!
+    
+    fileprivate let spacingBetweenCell: CGFloat = 10.0
+    fileprivate let countCellInRow = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let nib = UINib.init(nibName: "PhotoCollectionViewCell", bundle: nil)
+        photoCollectionView.register(nib, forCellWithReuseIdentifier: "PhotoCollectionViewCell")
     }
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as! PhotoCollectionViewCell
+        return cell
+    }
+}
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let widthSize = self.photoCollectionView.bounds.size.width/CGFloat(countCellInRow) - spacingBetweenCell
+        return CGSize(width: widthSize, height: widthSize)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spacingBetweenCell
+    }
 }
 
